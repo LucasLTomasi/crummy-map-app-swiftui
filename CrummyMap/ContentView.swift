e6b07daa9dc42c2cec553569fd31d95a8bbeea1d
Lucas Tomasi <lucasltomasi@gmail.com>
2021-07-08 08:18:52 -0300

    Create project.

---
+++ CrummyMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrummyMap
+//
+//  Created by Lucas Tomasi on 08/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrummyMap/CrummyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrummyMapApp.swift
+//  CrummyMap
+//
+//  Created by Lucas Tomasi on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CrummyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
